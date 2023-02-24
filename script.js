7f096a0fddd07a9db09197295bbc20daff1972b4
Eric O. Beestrum <eric@beestrum.com>
2023-02-24 11:25:41 -0600

    Initial file set-up (html, js)

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
