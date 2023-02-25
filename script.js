8129c4be14e7e2631cc38a185e71cc786e56f15f
Eric O. Beestrum <eric@beestrum.com>
2023-02-25 09:44:50 -0600

    Player button choices completed, values returned correctly

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-console.log("Hello");
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const fired_button = button.value;
+    console.log(fired_button);
+  });
+});
 
+/*
+function playerChoice() {
+  let valueChoice = document.getElementById("choice").value;
+  console.log(valueChoice);
+  document.getElementById("result").innerHTML = valueChoice;
+}
+*/
+/* PLAYGROUND CODE
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  document.getElementById("demo").innerHTML = x;
+}
+*/
+
+/* SUPPLIED CODE...
 function playRound(playerSelection, computerSelection) {
   // your code here!
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+
 console.log(playRound(playerSelection, computerSelection));
+*/
