1dfa6a8a252619ee931ef6d90b971b7792638012
Eric O. Beestrum <eric@beestrum.com>
2023-02-25 19:46:25 -0600

    Set variables into array; auto-populate button instances with class, name, value, and innerHTML.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//SET VARIABLES
+const optionsArray = ["rock", "paper", "scissors"]; //SET VARIABLES
+const buttonCount = document.getElementsByClassName("button"); //GET # OF BUTTONS
+
+//POPULATE BUTTONS W/ CLASS NAME
+for (let i = 0; i < optionsArray.length; i++) {
+  //COUNT .button INSTANCES
+  document.getElementsByClassName("button")[i].classList.add(optionsArray[i]);
+  //ASSIGN NAME
+  document.getElementsByClassName("button")[i].name = optionsArray[i];
+  //ASSIGN VALUE
+  document.getElementsByClassName("button")[i].value = optionsArray[i];
+  //ASSIGN BUTTON TEXT
+  document.getElementsByClassName("button")[i].innerHTML = optionsArray[i];
+}
 //PLAYER/BUTTON LISTENER
 document.querySelectorAll(".button").forEach((button) => {
   button.addEventListener("click", () => {
@@ -9,7 +24,7 @@ document.querySelectorAll(".button").forEach((button) => {
 
 //COMPUTER RANDOM CHOICE
 function nextStep() {
-  console.log('CompChoice')
+  console.log("CompChoice");
 }
 
 /* SUPPLIED CODE...
