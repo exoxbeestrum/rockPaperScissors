db7f71fd3446792f7766fbf7083848daaaa249d3
Eric O. Beestrum <eric@beestrum.com>
2023-02-26 11:57:39 -0600

    playRound() Fixed, Scorecard added, First-to-Five wins.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//SET GLOBAL VARIABLES
+//SET VARIABLES
 const optionsArray = ["rock", "paper", "scissors"]; //SET VARIABLES
 const buttonCount = document.getElementsByClassName("button"); //GET # OF BUTTONS
+
 let playerSelection = null;
 let computerSelection = null;
-
+let playerScore = 0;
+let computerScore = 0;
 
 //POPULATE BUTTONS W/ CLASS NAME
 for (let i = 0; i < optionsArray.length; i++) {
@@ -16,6 +18,10 @@ for (let i = 0; i < optionsArray.length; i++) {
   //ASSIGN BUTTON TEXT
   document.getElementsByClassName("button")[i].innerHTML = optionsArray[i];
 }
+
+//SET THE SCORE
+document.getElementById("player-score").innerHTML = playerScore;
+document.getElementById("comp-score").innerHTML = computerScore;
 
 //PLAYER/BUTTON LISTENER
 document.querySelectorAll(".button").forEach((button) => {
@@ -36,20 +42,42 @@ function getComputerChoice(playerSelection) {
 function playRound(playerSelection, computerSelection) {
   let playerIndex = optionsArray.indexOf(playerSelection);
   let computerIndex = optionsArray.indexOf(computerSelection);
-  console.log("playr " + playerIndex);
-  console.log("comp " + computerIndex);
-  if (playerIndex > computerIndex) {
-    console.log("player win");
-  } else if (playerIndex < computerIndex) {
-    console.log("comp win");
-  } else {
+  console.log("playr " + playerSelection);
+  console.log("comp " + computerSelection);
+  //CHECKS optionsArray[2] DOES NOT BEAT optionsArray[0]; PLAYER SCORES
+  if (playerIndex > computerIndex && computerIndex - playerIndex != 2) {
+    playerScore++;
+  }
+  //CHECK optionsArray[2] DOES NOT BEAT optionsArray[0]; COMPUTER SCORES
+  if (playerIndex < computerIndex && playerIndex - computerIndex != 2) {
+    computerScore++;
+  }
+  //optionsArray[0] BEATS optionsArray[2]; PLAYER SCORES
+  if (computerIndex - playerIndex == 2) {
+    playerScore++;
+  }
+  //optionsArray[0] BEATS optionsArray[2]; COMPUTER SCORES
+  if (playerIndex - computerIndex == 2) {
+    computerScore++;
+  }
+  //CHECKS FOR TIES
+  if (playerIndex == computerIndex) {
     console.log("tie");
   }
-}
-/* SUPPLIED CODE...
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
+  //UPDATE SCORE CARD
+  evalScore(playerScore, computerScore);
 
-console.log(playRound(playerSelection, computerSelection));
-*/
+  //KEEP SCORE
+  function evalScore(playerScore, computerScore) {
+    document.getElementById("player-score").innerHTML = playerScore;
+    document.getElementById("comp-score").innerHTML = computerScore;
+    //DECLARES WINNER
+    if (playerScore == 5 || computerScore == 5) {
+      document.getElementById("win").innerHTML = "WIN!!!";
+      //DISABLE BUTTONS
+      for (let i = 0; i < optionsArray.length; i++) {
+        document.getElementsByClassName("button")[i].disabled = true;
+      }
+    }
+  }
+}
